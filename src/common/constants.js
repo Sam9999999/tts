21a58cc57c94a9498adaeeb5a4cebfb2ccba3903
Sam9999999 <97665186+Sam9999999@users.noreply.github.com>
2022-01-13 23:11:38 +0530

    Update constants.js

--- src/common/constants.js
+++ src/common/constants.js
@@ -1,9 +1,9 @@
 const MESSAGE_EMBED = {
   color: '#20B2AA',
-  langThumbnail: 'https://i.imgur.com/QbNXO4q.jpg',
-  langURL: 'https://github.com/moonstar-x/discord-tts-bot#language-support',
-  helpThumbnail: 'https://i.imgur.com/Tqnk48j.png',
-  helpURL: 'https://github.com/moonstar-x/discord-tts-bot/issues'
+  langThumbnail: 'https://discord.gg/FNG96drVfv',
+  langURL: 'https://discord.gg/FNG96drVfv',
+  helpThumbnail: 'https://discord.gg/FNG96drVfv',
+  helpURL: 'https://discord.gg/FNG96drVfv'
 };
 
 const EMBED_FIELD_MAX_SIZE = 1024;
